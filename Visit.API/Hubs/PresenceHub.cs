e1e6ca5f5e1b0bb0b1b75e0773805a16151d80a9
amiralivali <amiralivali123123@gmail.com>
2026-01-05 21:01:40 +0330

    last change about signalr

---
+++ Visit.API/Hubs/PresenceHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Visit.API.Hubs
+{
+    public class PresenceHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("Connect");
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Console.WriteLine("disconnect");
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- Visit.API/Program.cs
+++ Visit.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Visit.API.Hubs;
 using Visit.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 builder.Services.AddDbContext<VisitDbContext>();
 
@@ -20,6 +21,7 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();   
 }
 
+app.MapHub<PresenceHub>("/presencehub");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
